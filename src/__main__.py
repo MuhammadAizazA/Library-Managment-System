a16bd883f8c47eabcf8688ce1837231628319231
MuhammadAizazA <aizaz.ali.shah.0205@gamil.com>
2023-12-08 12:05:31 +0500

    Cleaning

--- src/__main__.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-if __name__=='__main__':
-    csv_file = open('Data/admin.csv','r')
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        print(row[0])
-    csv_file.close()
